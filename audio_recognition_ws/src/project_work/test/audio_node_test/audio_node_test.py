3fa731f20d5b78d97ccf9ad2d7f28d272690fab8
saramassa <saramassaro2500@gmail.com>
2024-01-16 18:20:21 +0100

    test audio node

--- audio_recognition_ws/src/project_work/test/audio_node_test/audio_node_test.py
+++ audio_recognition_ws/src/project_work/test/audio_node_test/audio_node_test.py
@@ -1,115 +1,142 @@
 #!/usr/bin/python3
 import os
-import json
-TEST_PATH = os.path.dirname(os.path.abspath(__file__))
-import time
-from std_msgs.msg import Bool, String
-import rospy
 from config import *
+from std_msgs.msg import Int16MultiArray, Bool
+import rospy
+import json
+import time
+TEST_PATH = os.path.dirname(os.path.abspath(__file__))
+from gtts import gTTS
+from playsound import playsound
 
-class AudioNodeTest:
+
+class AudioNodeTest(object):
     def __init__(self):
-        super().__init__()
-        self._text = ""
-        self._PEPPER_TALK = False
-        self._HUMAN_PRESENCE = False
         self._output = None
         self._groundtruth = None
-        self._pub_human_presence = rospy.Publisher(HUMAN_PRESENCE_TOPIC, Bool, queue_size=0)
-        #self._pub_pepper_talk = rospy.Publisher(PEPPER_TALK_TOPIC, Bool, queue_size=0)
-        self._pub_output_chatbot = rospy.Publisher(CHATBOT_OUTPUT_TOPIC, String, queue_size=0)
-    
-    def _setup(self, test_case_path):
-        """Load groundtruth from file stored in specific test_case_path, with name groundtruth.txt.
-        for each file groundtruth.txt there is a single value True or False.
-        Set the value of output to False.
-        Args:
-            test_case_path (_type_): _description_
+        self._pub_pepper_talk = rospy.Publisher(PEPPER_TALK_TOPIC, Bool, queue_size=0)      
+        self._pub_human_presence = rospy.Publisher(HUMAN_PRESENCE_TOPIC, Bool, queue_size=0)      
+        self._PEPPER_TALK=False
+        self._HUMAN_PRESENCE=False
+        self._set_output(False)
+
+    def _setup(self,test_case_path):
         """
-        self._output = False
-        # check if the file exists
-        if not os.path.isfile(os.path.join(test_case_path, "groundtruth.txt")):
-            raise FileNotFoundError("File groundtruth.txt not found in {}".format(test_case_path))
-        with open(os.path.join(test_case_path, "groundtruth.txt"), "r") as f:
+        Load groundtruth from file stored in specific test_case_path.
+        Args:
+            test_case_path (String)
+        """
+        with open(os.path.join(test_case_path,"groundtruth.txt"),"r") as f:
             g = f.read()
-        self._groundtruth = g=="True"
-    
+        if g=="False":
+            self._groundtruth=False
+        if g=="":
+            self._groundtruth="Groundtruth empty"
+        if g=="True":
+            self._groundtruth=True
+        return
+
     def _cleanup(self):
-        """Reset groundtruth and detector output"""
+        """
+        Reset groundtruth and detector output
+        """
         self._groundtruth = None
-        self._output = False
-    
+        self._output = None
+
     def _test(self):
-        """Test the correctness of the detector output"""
+        """
+        Test the correctness of the detector output
+        """
         # check if the sequece 
-        if self._groundtruth == self._output:
-            print("Passed")
+        if self._get_groundtruth()=="Groundtruth empty":
+            print("Groundtruth empty")
         else:
-            print("Failed")
-     
+            if self._output == self._groundtruth:
+                print("Passed")
+            else:
+                print("gt: ")
+                print(self._get_groundtruth())
+                print("output: ")
+                print(self._get_output())
+                print("Failed")
+    
+    def _get_output(self):
+        """
+        Return detector output
+        """
+        return self._output
+    
+    def _set_output(self,output):
+        """
+        Set detector output
+        """
+        self._output=output
+    
+    def _get_groundtruth(self):
+        """
+        Return groundtruth
+        """
+        return self._groundtruth
+
     def __read_config(self, file_path):
         try:
             with open(file_path, 'r') as json_file:
                 data = json.load(json_file)
                 # Imposta le variabili in base ai valori nel file JSON
-                self._PEPPER_TALK = data.get("TEST_PEPPER_TALK", False)
+                self._PEPPER_TALK = data.get("TEST_PEPEPER_TALK", False)
                 self._HUMAN_PRESENCE = data.get("TEST_HUMAN_PRESENCE", False)
         except FileNotFoundError:
             print(f"File '{file_path}' non trovato. Impostazione di valori predefiniti.")
 
-    def __get_output_chatbot_from_file(self, file_path, file_name):
-        #Leggi il file txt e ritorna il contenuto
-        
-        file_path = os.path.join(file_path, file_name)
-        with open(file_path, 'r') as file:
-            output = file.read()
-        return output
 
-    
-    
-    
-    def __test_case(self, test_case_folder):
+    def _test_case(self,test_case_folder):
         print(test_case_folder.upper()+":",end="\t")
         test_case_path = os.path.join(TEST_PATH,test_case_folder)
         # get groundtruth
         self._setup(test_case_path)
-        # test text
+
         text_path = test_case_path
         self.__read_config(os.path.join(text_path, "config_test.json"))
+        #pubblico sui topic, il nodo di audio_node leggerà questi valori
+        self._pub_pepper_talk.publish(Bool(self._PEPPER_TALK ))
+        self._pub_human_presence.publish(Bool(self._HUMAN_PRESENCE))
+
+        #audio_path=os.path.join(test_case_path,"test_audio.wav")
         
-        if self._HUMAN_PRESENCE:
-            self._pub_human_presence.publish(True)
-        else:
-            self._pub_human_presence.publish(False)
-        
-        if self._PEPPER_TALK:
-            output = self.__get_output_chatbot_from_file(text_path, "text_data.txt")
-            self._pub_output_chatbot.publish(output)
-        
-        time.sleep(5)    
-        
-        
+        text="Hello Pepper"
+        to_speak = gTTS(text=text, lang=LANGUAGE, slow=False)
+        to_speak.save("temp.wav")
+        playsound("temp.wav")
+        os.remove("temp.wav")
+
+
+        time.sleep(10)#in questo modo il nodo di audio_node ha il tempo di pubblicare l'audio sul topic
+        if (self._get_output()==None): #se è None allora non ho eseguito la callback _audio_user
+            self._set_output(False)
         self._test()
-        self._cleanup()
-        
-    def __handle_user_input(self, data):
+        self._cleanup()     
+
+
+    def _audio_user(self, audio):
         self._set_output(True)
-        print("User input: {}".format(data.data))
-    
+
     def start(self):
         rospy.init_node('audio_node_test', anonymous=True)
-        rospy.Subscriber(USER_INPUT_TOPIC, String, self.__handle_user_input)
+        rospy.Subscriber(USER_AUDIO_TOPIC, Int16MultiArray,self._audio_user)
+    
         test_cases = os.listdir(TEST_PATH)
         test_cases.sort()
+        
         for test in test_cases:
-            if test!="__pycache__" and test!="base_test.py" and test!="audio_node_test.py" and test!=".gitignore" and test!=".git":
+            if test!="__pycache__" and test!="audio_node_test.py" and test!="config.py":
+                #if (test=="test_case_3"):
                 if not os.path.isfile(os.path.join(TEST_PATH,test)):
-                    self.__test_case(test)
-                    
+                    self._test_case(test)
         print("TEST FINISHED")
         print("Type CTRL+C to exit")
         while not rospy.is_shutdown():
             pass
+        
 if __name__ == "__main__":
     try:
         d = AudioNodeTest()
