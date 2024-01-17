b349bbbbd43041acd7a1c3ae0e8b0811a85cf0e1
Antonio Nocerino <nocerinoantonio99@gmail.com>
2024-01-17 09:25:38 +0100

    .

--- audio_recognition_ws/src/project_work/scripts/config.py
+++ audio_recognition_ws/src/project_work/scripts/config.py
@@ -7,7 +7,7 @@ CALIBRATION_TIME = 0.2
 
 # Flag: if False, the working of the chatbot will be simulated with the command line.
 # -- In the use must be True.
-CHATBOT_RUNNING = False
+CHATBOT_RUNNING = True
 
 # Size of the time window for the computation of the mel spectrogram
 # -- 1024 is good for the pepper microphone
--- optimized_ws/src/rasa_ros/scripts/rasa_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BOT_DIR="/home/satoshinakamoto/CognitiveRobotics/Bot_Cognitive_ChatGpt"
-
-cd $BOT_DIR
-
-rasa run -m models --endpoints endpoints.yml --port 5002 --credentials credentials.yml --enable-api
