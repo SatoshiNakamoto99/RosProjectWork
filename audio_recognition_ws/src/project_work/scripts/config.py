5e4023f5763582bd26095dba1276d566e32e5632
Antonio Nocerino <nocerinoantonio99@gmail.com>
2024-01-17 09:57:25 +0100

    .

--- audio_recognition_ws/src/project_work/scripts/config.py
+++ audio_recognition_ws/src/project_work/scripts/config.py
@@ -7,18 +7,17 @@ CALIBRATION_TIME = 0.2
 
 # Flag: if False, the working of the chatbot will be simulated with the command line.
 # -- In the use must be True.
-CHATBOT_RUNNING = True
+CHATBOT_RUNNING = False
 
 # Size of the time window for the computation of the mel spectrogram
-# -- 1024 is good for the pepper microphone
 CHUNK_SIZE = 1024
 
 
 # This parameter is the minimum confidence to the detection.
-FACE_MIN_DETECTION_CONFIDENCE = 0.4
+FACE_MIN_DETECTION_CONFIDENCE = 0.8
 
 # The maximum number of frame with different state before changing that
-HUMAN_PRESENCE_GHOST_FRAME = 15
+HUMAN_PRESENCE_GHOST_FRAME = 5
 
 # Topic in witch a std_msgs/Bool will be published if an human is present or not. 
 HUMAN_PRESENCE_TOPIC = '/track/human_presence'
@@ -29,6 +28,10 @@ LANGUAGE='en-GB'
 
 # pyAudio microphone idex. That need to be changed basing on the device. 
 MICROPHONE_INDEX = None
+
+
+#If Model Classe is True I load the model opencv else resnet + ssd
+MODEL_CLASSE = True
 
 # Flag: If False, the services who need Pepper are not called. Useful for debug.
 # -- In the use must be True.
@@ -43,30 +46,31 @@ SAVE_RAW_AUDIO = False
 SAVE_RAW_FRAME = False
 
 # Audio sample rate
-# -- 16000 Good for the pepper microphone
 RATE = 16000
 
 
 # Path of scripts folder obtained dinamically.
 REF_PATH = os.path.dirname(os.path.abspath(__file__))
 
-# User input audio topic
+# User input audio topic with std_msgs/Int16MultiArray
 USER_AUDIO_TOPIC = '/audio/user_input'
 
-# User input text topic
+# User input text topic with std_msgs/String
 USER_INPUT_TOPIC = '/chatbot/input'
 
-# Chatbot output text topic
+# Chatbot output text topic with std_msgs/String
 CHATBOT_OUTPUT_TOPIC = '/chatbot/output'
 
-# Pepper Talk Topic
+# Pepper Talk Topic with std_msgs/Bool
 PEPPER_TALK_TOPIC = '/pepper/talk'
 
 # The fps published by the image publisher. This must be replicated in 
 # pepper_nodes
 VIDEO_FPS = 10
 
-# Topin on which will be published the image received by the Pepper camera
+# Topic on which will be published the image received by the Pepper camera
 VIDEO_TOPIC = '/in_rgb'
+
+
 
 VERBOSE = False
--- audio_recognition_ws/src/project_work/scripts/video_detector.py
+++ audio_recognition_ws/src/project_work/scripts/video_detector.py
@@ -11,21 +11,108 @@ from std_msgs.msg import Bool
 from threading import Lock
 
 
+# class VideoDetector(object):
+#     """This node see if an human was present and publish that presence on that topic."""
+
+#     def __init__(self):
+#         """Load the model and initalize the publisher."""
+#         protoPath = os.path.sep.join([REF_PATH, 'face_detection_model', "deploy.prototxt"])
+#         modelPath = os.path.sep.join([REF_PATH, 'face_detection_model', "res10_300x300_ssd_iter_140000.caffemodel"])
+#         self._detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+
+#         self._publisher = rospy.Publisher(HUMAN_PRESENCE_TOPIC, Bool, queue_size=15)
+
+#         self._curr_state = False
+#         self._change_state_count = 0
+
+#         self._mutex_net = Lock()
+
+#     def _handle_frame(self, msg):
+#         """Callback function. Receive an image and use a detector to verify if almost one faces is in the image.
+#         This method is thread-safe."""
+#         # Obtain a frame from the rospy message
+#         frame = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
+
+#         # if ON_PEPPER:
+#         #     frame = frame[:,:,::-1]
+
+#         if SAVE_RAW_FRAME:
+#             if not os.path.exists(os.path.join(REF_PATH, 'saved_imgs')):
+#                 os.mkdir(os.path.join(REF_PATH, 'saved_imgs'))
+#             cv2.imwrite(os.path.join(REF_PATH, 'saved_imgs', f'{datetime.now().strftime("%m-%d-%Y-%H-%M-%S.%f")}.jpeg'), frame)
+       
+#         # Transform the image in a blob
+#         imageBlob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0), swapRB=False, crop=False)
+        
+#         # Make the prediction
+#         self._mutex_net.acquire()
+#         self._detector.setInput(imageBlob)
+#         detections = self._detector.forward()
+#         self._mutex_net.release()
+        
+#         # we ensure to have at least one face
+#         if len(detections) > 0:
+#             # we're making the assumption that each image has only ONE face, so find the bounding 
+#             # box with the largest probability
+#             i = np.argmax(detections[0, 0, :, 2])
+#             confidence = detections[0, 0, i, 2]
+
+#             # ensure that the detection with the largest probability also means our minimum probability 
+#             # test (thus helping filter out weak detections)
+#             if confidence > FACE_MIN_DETECTION_CONFIDENCE:
+#                 self._publish_state(True)
+#             else: 
+#                 self._publish_state(False)
+#         else:
+#             self._publish_state(False)
+
+#     def _publish_state(self, new_state):
+#         """Publish the new state just if changed"""
+#         if new_state != self._curr_state:
+#             if self._change_state_count >= HUMAN_PRESENCE_GHOST_FRAME:
+#                 self._publisher.publish(Bool(new_state))
+#                 self._curr_state = new_state
+#             else:
+#                 self._change_state_count += 1
+#         else:
+#             self._change_state_count = 0
+#             #self._publisher.publish(Bool(new_state))
+
+
+#     def start(self):
+#         """Init the node and subscribe to the video input"""
+#         rospy.init_node('video_detection', anonymous=True)
+#         rospy.Subscriber(VIDEO_TOPIC, Image, self._handle_frame)
+#         rospy.spin()
+
+
+# if __name__ == '__main__':
+#     my_node = VideoDetector()
+#     my_node.start()
 class VideoDetector(object):
     """This node see if an human was present and publish that presence on that topic."""
 
-    def __init__(self):
+    def __init__(self, model_classe= True ):
         """Load the model and initalize the publisher."""
-        protoPath = os.path.sep.join([REF_PATH, 'face_detection_model', "deploy.prototxt"])
-        modelPath = os.path.sep.join([REF_PATH, 'face_detection_model', "res10_300x300_ssd_iter_140000.caffemodel"])
-        self._detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
-
+        
+        
+        if model_classe:
+            protoPath = os.path.sep.join([REF_PATH, 'face_detection_model', "opencv_face_detector.pbtxt"])
+            modelPath = os.path.sep.join([REF_PATH, 'face_detection_model', "opencv_face_detector_uint8.pb"])
+            self._detector = faceNet = cv2.dnn.readNet(protoPath,modelPath)
+        else:
+            protoPath = os.path.sep.join([REF_PATH, 'face_detection_model', "deploy.prototxt"])
+            modelPath = os.path.sep.join([REF_PATH, 'face_detection_model', "res10_300x300_ssd_iter_140000.caffemodel"])
+            self._detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+            
+        
+        
         self._publisher = rospy.Publisher(HUMAN_PRESENCE_TOPIC, Bool, queue_size=15)
 
         self._curr_state = False
         self._change_state_count = 0
 
-        self._mutex_net = Lock()
+        
 
     def _handle_frame(self, msg):
         """Callback function. Receive an image and use a detector to verify if almost one faces is in the image.
@@ -33,22 +120,23 @@ class VideoDetector(object):
         # Obtain a frame from the rospy message
         frame = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
 
-        # if ON_PEPPER:
-        #     frame = frame[:,:,::-1]
-
+        
         if SAVE_RAW_FRAME:
             if not os.path.exists(os.path.join(REF_PATH, 'saved_imgs')):
                 os.mkdir(os.path.join(REF_PATH, 'saved_imgs'))
             cv2.imwrite(os.path.join(REF_PATH, 'saved_imgs', f'{datetime.now().strftime("%m-%d-%Y-%H-%M-%S.%f")}.jpeg'), frame)
        
-        # Transform the image in a blob
-        imageBlob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0), swapRB=False, crop=False)
+        frameOpencvDnn = frame.copy()
+        frameHeight = frameOpencvDnn.shape[0]
+        frameWidth = frameOpencvDnn.shape[1]       
+       
+        
+        blob = cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
         
         # Make the prediction
-        self._mutex_net.acquire()
-        self._detector.setInput(imageBlob)
+        
+        self._detector.setInput(blob)
         detections = self._detector.forward()
-        self._mutex_net.release()
         
         # we ensure to have at least one face
         if len(detections) > 0:
@@ -87,5 +175,5 @@ class VideoDetector(object):
 
 
 if __name__ == '__main__':
-    my_node = VideoDetector()
+    my_node = VideoDetector(MODEL_CLASSE)
     my_node.start()
